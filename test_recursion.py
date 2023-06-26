1fc4e298962fbd26493273d84f2b7909a76c3f04
tkwr565 <tkwr565@gmail.com>
2023-06-27 03:34:02 +0800

    update test_recursion

--- test_recursion.py
+++ test_recursion.py
@@ -1,3 +1,7 @@
+# This is the unittest module to compare the recursion version against the imperative version
+# unittest was done for the algorithm calculation function, and the print function
+# test was done for 3x3, 4x4 and 6x6 matrices
+
 import unittest
 import version_imperative
 import version_recursion
@@ -11,6 +15,12 @@ class FloydWarshallTestCase(unittest.TestCase):
         Set up the test case with sample data
         """
         self.graph = [[0, 5, INF], [INF, 0, 3], [INF, INF, 0]]
+        self.graph0 = [
+            [0, INF, 9, 3],
+            [3, 0, 6, 2],
+            [INF, 2, 0, INF],
+            [INF, 5, INF, 0],
+        ]
         self.graph1 = [
             [0, 2, 7, 9, 1, INF],
             [INF, 0, INF, 1, 9, 2],
@@ -26,7 +36,15 @@ class FloydWarshallTestCase(unittest.TestCase):
         and imperative version
         """
 
-        # first test with 3x3 sample
+        # first test with 4x4 sample
+        version_imperative.V = 4
+        version_recursion.V = 4
+        expected_output = version_imperative.floydWarshallImperative(self.graph0)
+        dist = [row[:] for row in self.graph0]
+        result = version_recursion.floydWarshallRecursive(self.graph0, dist, 0, 0, 0)
+        self.assertEqual(result, expected_output)
+
+        # then test with 3x3 sample
         version_imperative.V = 3
         version_recursion.V = 3
         expected_output = version_imperative.floydWarshallImperative(self.graph)
@@ -52,6 +70,19 @@ class FloydWarshallTestCase(unittest.TestCase):
         # use redirect_stout to redirect print value to StringIO object for unittest
         import io
         from contextlib import redirect_stdout
+
+        # first test with 4x4 sample
+        version_imperative.V = 4
+        version_recursion.V = 4
+        output = io.StringIO()
+        with redirect_stdout(output):
+            version_recursion.printSolutionRecursive(self.graph0)
+
+        expected_output = io.StringIO()
+        with redirect_stdout(expected_output):
+            version_imperative.printSolution(self.graph0)
+
+        self.assertEqual(expected_output.getvalue(), output.getvalue())
 
         # first test with 3x3 sample
         version_imperative.V = 3
